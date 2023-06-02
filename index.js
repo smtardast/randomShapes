34f54b9bcfe893a52451211095ce785d42936a1e
smtardast <43958348+smtardast@users.noreply.github.com>
2023-06-02 21:53:17 +0000

    on click change bacteria color

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ var layer = new Konva.Layer();
 
 stage.add(layer);
 var nr = Math.random()*10;
+var ax = stage.width() * Math.random();
+var ay = stage.height() * Math.random();
+var wide = stage.width() ;
 //create enclosure loop
 for (var i = 0; i < nr*3; i++) {
     
-    var ax = stage.width() * Math.random();
-    var ay = stage.height() * Math.random();
       
     enclosure = new Konva.Line({
     
@@ -49,13 +50,15 @@ var star;
           x: stage.width() * Math.random(),
           y: stage.height() * Math.random(),
           fill: 'blue',
-          numPoints: 10,
-          innerRadius: 20,
+          numPoints: 4,
+          innerRadius: 10,
           outerRadius: 25,
           draggable: true,
           name: 'star ' + i,
           shadowOffsetX: 5,
           shadowOffsetY: 5,
+          shadowColor: 'yellow',
+          shadowBlur: 0.5,
         });
         layer.add(star);
       }
@@ -63,37 +66,73 @@ var star;
 
 //making random amounts of circles
 
-var star;
-      for (var i = 0; i < nr/2; i++) {
-        star = new Konva.Circle({
+var circle;
+      for (var i = 0; i < stage.width() * Math.random()/300; i++) {
+        circle = new Konva.Circle({
           x: stage.width() * Math.random(),
           y: stage.height() * Math.random(),
-          fill: 'blue',
-          radius: Math.random()*40,
+          fill: couleurs[i],
+          radius: (stage.width() * Math.random())/10,
           draggable: true,
-          name: 'star ' + i,
-          shadowOffsetX: 5,
-          shadowOffsetY: 5,
+          name: 'circle ' + i,
+          opacity: Math.random(),
+
         });
-        layer.add(star);
+        layer.add(circle);
     }
+
+    // making randow amounts of rings
+
+    var ring;
+    for (var i = 0; i < stage.width() * Math.random()/300; i++) {
+      var ringRad = (stage.width() * Math.random());
+
+      star = new Konva.Ring({
+        x: stage.width() * Math.random(),
+        y: stage.height() * Math.random(),
+        fill: couleurs[i],
+        innerRadius:  ringRad/18,
+        outerRadius: ringRad/10,
+        draggable: true,
+        name: 'ring ' + i,
+        opacity: Math.random(),
+       
+      });
+      layer.add(star);
+  }
     //Making bacteria
       var bacteria;
-for (var i = 0; i < nr*5; i++) {
-   
+      console.log(stage.width() * Math.random());
+      console.log(stage.width());
+      console.log(ax);
+  
+for (var i = 0; i < 10; i++) {
+ var couleur = 0;
+  couleur++;
+  if (couleur == 10){
+    couleur = 0
+  }
     bacteria = new Konva.Line({
-    
         x: stage.width() * Math.random(),
-          y: stage.height() * Math.random(),
-        fill: couleurs[i],
+        y: stage.height() * Math.random(),
+        fill: couleurs[1],
         points: [Math.random()*20,Math.random()*20,Math.random()*40,Math.random()*20,Math.random()*40,Math.random()*40],       strokeWidth: 5,
         closed: true,
         tension: 1,
         draggable: true,
       });
     
+      var a = 0;
       // add the shape to the layer
       layer.add(bacteria);
+      bacteria.on('click', function(){
+        a++;
+        if (a == 10){
+          a = 0
+        }
+        var fill = this.fill() == couleurs[i] ? couleurs[a] : couleurs[i];
+        this.fill(fill);
+      })
 
 }
 //        points: [Math.random()*20,Math.random()*20,Math.random()*40,Math.random()*20,Math.random()*40,Math.random()*40,Math.random()*20,Math.random()*40,],
@@ -103,8 +142,6 @@ for (var i = 0; i < nr*5; i++) {
 var plus = new Konva.Line({
     points: [0,50,50,50,50,0,75,0,75,50,125,50,125,75,75,75,75,125,50,125,50,75,0,75, ],
     fill: '#00D2FF',
-    stroke: 'black',
-    strokeWidth: 5,
     closed: true,
     draggable: true,
   });
